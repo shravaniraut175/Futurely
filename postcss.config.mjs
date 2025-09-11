f7af8b760577a80e54288f952eade3f83fb3f3b5
Shravani <shravani.236327205@vcet.edu.in>
2025-09-11 15:35:00 +0530

    Initial commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-preset-env": {
+      stage: 1,
+      features: {
+        "color-functional-notation": true,
+        "lab-function": true,
+      },
+    },
+  },
 };
 
 export default config;
